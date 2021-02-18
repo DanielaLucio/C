efeff63f49e801bcca5ab2b0925bb164d91669fc
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-02-18 15:32:39 -0600

    Add files via upload

---
+++ Excercise_1.6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main (){
+
+
+  int c;
+
+
+  printf("\nIntroduzca un caracter, EOF inclusive\n");
+
+ ((c = getchar()) == EOF);
+
+  printf("%d\n");
+
+
+    return 0;
+ }
