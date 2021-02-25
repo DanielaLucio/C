698fdfc8ae7773259db38c0baec58721e6911ff0
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-02-25 13:10:40 -0600

    Add files via upload

--- Homework/homework.1.c
+++ Homework/homework.1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+/* this program is going to read an HTML text and convert it to plain text also
+replace spaces  to underscore in header and bold faced text*/
 
 int main (){
     int c;
-    char strval[90];
-    char val1[4]={"<h1>"};
-    char val2[8]={"<strong>"};
-    int i, d,  a;
-    int sw, sw2,sw3;
+    char strval[90];//array to validate the current tag
+    char val1[4]={"<h1>"};//tags to validate
+    char val2[8]={"<strong>"};//tags to validate
+    int i, d,  a;// these are the variables of the cycles
+    int sw, sw2,sw3;//these are to turn on or off the conditions, sw is for putchar or not putchar, sw2 is for replace  or no replace tag 1 and sw3 same as sw2 but for tag 2
+    //switches start
     sw = 0;
     sw2 = 0;
     sw3 = 0;
@@ -24,7 +27,7 @@ int main (){
             }
         }
 
-        if (sw == 0){
+        if (sw == 0){//here enters when its not a tag
             if (sw2 == 0 && sw3 == 0){
                 putchar(c);
             }
@@ -36,7 +39,7 @@ int main (){
                 }
 
         }
-        else{
+        else{//here storage the current tag
             strval[d]= c;//storage the character to validated it
             d++;
         }
@@ -52,7 +55,7 @@ int main (){
             }
              for (i=0;i<8;i++){
                 sw3 = 1;
-                if (strval[i]!= val2[i]){
+                if (strval[i]!= val2[i]){//is going to validate each character to check if are not the tag
                     sw3 = 0;
                 }
             }
