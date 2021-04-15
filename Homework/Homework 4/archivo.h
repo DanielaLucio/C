b2aff86f3b7bd0d5e06d143b74e078caabe994ee
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-04-15 11:47:58 -0500

    Add files via upload

---
+++ Homework/Homework 4/archivo.h
@@ -0,0 +1,6 @@
+#define NUMBER '0' /* signal that a number was found */
+int getop(char []);// determine if its a number
+void push(double);
+void SaveVar();
+double pop(void);
+void variable (double);
