5a2f3c6d13cf49bbd9a89239232ee3e17f8e51f8
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-04-29 14:02:35 -0500

    Create archivo.h

---
+++ Homework/homework6/archivo.h
@@ -0,0 +1,16 @@
+#define MAX 100
+char copy(char name);
+int getdata(char *user, char *line);
+char *strstr(char*, char*);
+
+typedef struct treenode {
+  char *username;
+  char *password;
+  struct treenode *left;
+  struct treenode *right;
+} node;
+
+node* insert(char *name, char *password, node* pos);
+void alpha(node* position);
+void deleteUser();
+void del(node* position);
