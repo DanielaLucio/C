1b47b5c2258f031dc38322564a65e02212452aaa
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-05-06 12:15:51 -0500

    Add files via upload

---
+++ Homework/\homework_7/archivo (1).h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define UNAME 8
+#define Work 13
+#define MAX 100
+ typedef struct{
+     int pin;
+     int entrada_salida;
+     char *fecha;
+     char *hora;
+     int mes;
+ }workers;
+ workers *trab;
+ 
+ void pin_registration();
+ void attendance_list(int t);
+ void timecome(int pin, char t);
+ int difftimes(int pin);
+ void reports();
+ void copy (char temp[], int i);
+ 
