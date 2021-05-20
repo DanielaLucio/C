7a71f340b1a9ca6599dfd47ab4eac7f769ef8b27
DanielaLucio <78566377+DanielaLucio@users.noreply.github.com>
2021-05-19 19:19:14 -0500

    Create archivo.h

---
+++ PIA/code/archivo.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct client //in this struct I storage the data of clien.txt
+{
+  int account [3] ;
+  int nip[3] ;
+  int status[3] ;
+ 
+} seek;
+
+struct amount//here I storage the data of amount.txt
+{
+  int account[3];           
+  int amount[3];
+  
+} view;
+
+int menu();
+void balance(int acc);
+void changepin(int nip, int acc);
+void withdrawal(int acc);
+void deposite(int acc);
